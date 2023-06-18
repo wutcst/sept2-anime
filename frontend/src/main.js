07a37bef46a87574c6d3e9a362d533909e1c3035
yiligo <nano@88.com>
2023-06-18 22:55:32 +0800

    feaq:引入Axios依赖,准备对接后端API

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from 'axios'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+
+
 
 app.mount('#app')
