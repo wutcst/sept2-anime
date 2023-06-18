bdda19c459edd78af981ee2cac13cfb6589dbdd4
yiligo <nano@88.com>
2023-06-18 11:15:02 +0800

    fix:修复地图组件的穿墙bug

--- frontend/src/components/GameMap.vue
+++ frontend/src/components/GameMap.vue
@@ -5,39 +5,39 @@
 export default {
   data() {
     return {
+      BG:0,
+      WALL:1,
+      PLAYER:2,
       //背景图片地址,尺寸30*30
       bg:[
         'https://s1.imgcap.xyz/ebbab847b15009dd4b39e9005616ee23.png',
         'https://s1.imgcap.xyz/aa7da080de92d8189a1dde2a657bfc71.png',
         'https://s1.imgcap.xyz/47a8e45d64b1117f4807d23268aabdc1.png'
       ],
-      //存储地图的9*9二维数组,0为背景,1为墙壁
-      map:Array.from({ length: 9 }, (_, rowIndex) =>
-          Array.from({ length: 9 }, (_, colIndex) => this.getItemType(rowIndex + 1, colIndex + 1))
-      ),
       //玩家的[行,列]坐标
-      user:[5,5]
+      player:[5,5],
     }
   },
   computed: {
-    //玩家的CSS计算属性,通过绝对定位显示在地图上
-    userStyle(){
-      return{
-        position: 'absolute',
-        top: (this.user[0]-1)*3 + 'vw',
-        left: (this.user[1]-1)*3 + 'vw',
-      };
+    //存储地图的9*9二维数组,0为背景,1为墙壁,2为玩家
+    map(){
+      return Array.from({ length: 9 }, (_, rowIndex) =>
+          Array.from({ length: 9 }, (_, colIndex) => this.getItemType(rowIndex + 1, colIndex + 1))
+      );
     }
   },
   methods: {
     /**
-     * 获取地图上元素的类型,0为背景,1为墙壁
+     * 获取地图上元素的类型,0为背景,1为墙壁,2为玩家
      * @param row
      * @param col
      * @returns {number}
      */
     getItemType(row,col){
-      if(row ===1 ||row === 9||col === 1||col === 9) {
+      if(row === this.player[0] && col === this.player[1]){
+        return 2;
+      }
+      else if(row ===1 ||row === 9||col === 1||col === 9) {
         if (row === 5 || col === 5) {
           return 0;
         } else {
@@ -53,19 +53,28 @@ export default {
      * @param event
      */
     handleKeyDown(event) {
+      event.preventDefault();
+      let newPlayer=Array.from(this.player);
       switch (event.keyCode) {
         case 37: // 左箭头键
-          this.user[1]--; //玩家列坐标减一
+          newPlayer[1]--; //玩家列坐标减一
           break;
         case 38: // 上箭头键
-          this.user[0]--;
+          newPlayer[0]--;
           break;
         case 39: // 右箭头键
-          this.user[1]++;
+          newPlayer[1]++;
           break;
         case 40: // 下箭头键
-          this.user[0]++;
+          newPlayer[0]++;
           break;
+      }
+      //判断是否撞墙
+      if(this.getItemType(newPlayer[0],newPlayer[1])==this.WALL){
+        return;
+      }
+      else{
+        this.player=newPlayer;
       }
     }
   },
@@ -80,21 +89,20 @@ export default {
 </script>
 
 <template>
-  <div class="map">
+  <div class="map" v-loading="false"
+       element-loading-text="正在切换洞穴"
+       element-loading-background="rgba(122, 122, 122, 0.8)">
     <div v-for="row in map" class="row">
-      <span v-for="item in row">
-        <img :src="bg[item]" />
-      </span>
+      <img v-for="item in row" :src="bg[item]" />
     </div>
-    <!--通过绝对定位让玩家图片在地图上显示-->
-    <img :src="bg[2]" :style="userStyle" />
   </div>
 </template>
 
 <style scoped>
 .map{
-  width:27vw;
-  display: flex;
+  width:28vw;
+  height: 28vw;
+  display: inline-flex;
   position: relative;
   flex-direction: row;
   flex-wrap:wrap;
