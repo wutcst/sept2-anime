3d004c6f522ec82d70bf302b39570a1bbd7e99d3
Ducking <1125829655@qq.com>
2023-06-24 19:34:16 +0800

    测试

--- src/test/java/com/example/worldofzuul/WorldofzuulApplicationTests.java
+++ src/test/java/com/example/worldofzuul/WorldofzuulApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.worldofzuul;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class WorldofzuulApplicationTests {
 
     @Test
     void contextLoads() {
+        SpringApplication.run(WorldofzuulApplicationTests.class);
     }
 
 }
